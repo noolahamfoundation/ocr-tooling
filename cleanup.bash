799633a3a76eb74f4c70bf6007d04e17b6e9d401
Nat Kanthan <natkeeran@gmail.com>
2016-10-19 21:23:20 -0400

    cleanup script

---
+++ cleanup.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Removes tmp files after batch processing
+
+rm -rf text_for*
+rm -rf OCR-*
+rm -rf text-*
+rm -rf page_*.log
+rm -rf page_*.upload
+rm -rf page_*.pdf
+rm -rf page_*.txt
+rm -rf pg_*.pdf
